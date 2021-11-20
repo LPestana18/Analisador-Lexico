62ff94dce3f4b16e643269c903a225282d987377
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-11-20 17:14:54 -0300

    create nextToken()

--- src/main/java/br/com/lucaspestana/compiler/lexico/IsiScanner.java
+++ src/main/java/br/com/lucaspestana/compiler/lexico/IsiScanner.java
@@ -8,17 +8,60 @@ public class IsiScanner {
 
     private char[] content;
     private int estado;
+    private int pos;
 
     public IsiScanner(String filename) {
         try {
             String txtConteudo;
-            txtConteudo = Files.readString(Paths.get(filename));
+            txtConteudo = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
             System.out.println("DEBUG ----------");
             System.out.println(txtConteudo);
             System.out.println("----------------");
             content = txtConteudo.toCharArray();
+            pos = 0;
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public Token nextToken() {
+        char currentChar;
+        String term = "";
+        if (isEOF()) {
+            return null;
+        }
+        estado = 0;
+        while (true) {
+            currentChar = nextChar();
+            switch (estado) {
+                case 0:
+                    if (isChar(currentChar)) {
+                        term += currentChar;
+                        estado = 1;
+                    } else if (isDigit(currentChar)) {
+                        estado = 3;
+                    } else if (isSpace(currentChar)) {
+                        estado = 0;
+                    } else if (isOperator(currentChar)) {
+                        estado = 5;
+                    } else {
+                        throw new RuntimeException("Unrecognized SYMBOL");
+                    }
+                    break;
+                case 1:
+                    if (isChar(currentChar) || isDigit(currentChar)) {
+                        estado = 1;
+                    } else {
+                        estado = 2;
+                    }
+                    break;
+                case 2:
+                    back();
+                    Token token = new Token();
+                    token.setType(Token.TK_IDENTIFIER);
+                    token.setText(term);
+                    return token;
+            }
         }
     }
 
@@ -36,5 +79,17 @@ public class IsiScanner {
 
     private boolean isSpace(char c) {
         return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+    }
+
+    private char nextChar() {
+        return content[pos++];
+    }
+
+    private boolean isEOF() {
+        return pos == content.length;
+    }
+
+    private void back() {
+        pos--;
     }
 }
--- src/main/java/br/com/lucaspestana/compiler/main/MainClass.java
+++ src/main/java/br/com/lucaspestana/compiler/main/MainClass.java
@@ -1,8 +1,20 @@
 package br.com.lucaspestana.compiler.main;
 
+import br.com.lucaspestana.compiler.lexico.IsiScanner;
+import br.com.lucaspestana.compiler.lexico.Token;
+
 public class MainClass {
 
     public static void main(String[] args) {
 
+        IsiScanner sc = new IsiScanner("src/main/java/input.isi");
+        Token token = null;
+
+        do {
+            token = sc.nextToken();
+            if (token != null) {
+                System.out.println(token);
+            }
+        } while (token != null);
     }
 }
